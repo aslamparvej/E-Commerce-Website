6d71da0ed904b90d79daf75237b5c196333dc281
Aslam Parvej <aslamparvej2@gmail.com>
2026-04-24 21:52:05 +0530

    Updated Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import ProductList from "../components/ProductList";
+import Features from "../components/Features";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ProductList />
+      <Features />
+    </>
+  );
+};
+
+export default Home;
