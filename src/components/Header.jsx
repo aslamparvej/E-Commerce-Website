973b3beaad969b32ad6b701df4514da8fd018455
Aslam Parvej <aslamparvej2@gmail.com>
2026-04-27 21:03:06 +0530

    Updated header and Topbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import Cart from "./Cart";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 bg-white p-4 border-b-[0.5px] border-gray-300">
+    <header className="sticky top-0 z-50 bg-white p-4 border-b-[0.5px] border-gray-300">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <h1 className="text-black font-bold text-2xl">Gadget Hunter</h1>
         <Navbar />
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,7 @@ const Topbar = () => {
   return (
     <div className="bg-black p-3 top-0 left-0 right-0">
       <p className="text-white text-center">
-        Summer Sale For All gadgets And Free Express Delivery - OFF 30%!{" "}
+        Summer Sale For All gadgets And Free Express Delivery - OFF 30%! (use code: SUMMER2026) {" "}
         <Link to="/" className="underline">Shop Now</Link>
       </p>
     </div>
